454d608faf53f53ff8665cb78aad68317b9677c1
Ndema Emmanuel Chidera <ndemaemmanuel82@gmail.com>
2024-08-17 10:35:36 -0700

    corrected post to get

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,6 @@ const router = require("express").Router()
 
 router.post("/signup",signup)
 router.post("/login",login)
-router.post("/all-users",fetchAllUsers)
+router.get("/all-users",fetchAllUsers)
 
 module.exports = router;
